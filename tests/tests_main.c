473e6b464ec218b11fb105aeff881feb0f076adc
Raffaele Fioratto <raffaele.fioratto@gmail.com>
2016-02-29 18:44:58 +0100

    Now tests can be built from CMake!

--- tests/tests_main.c
+++ tests/tests_main.c
@@ -1,5 +1,8 @@
 #include <check.h>
+
 #include <stdlib.h>
+#include <stdio.h>
+
 #include "test_suite.h"
 
 int main(void) {
@@ -9,7 +12,7 @@ int main(void) {
     sr = srunner_create(board_suite());
 
     //srunner_add_suite(sr, another_suite);
-
+    
     srunner_run_all(sr, CK_VERBOSE);
     number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
